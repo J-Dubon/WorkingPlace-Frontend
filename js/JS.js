fedb153ed02aeb2cddecafbc80acf737014e567c
Jose Dubon <josemdubonm@yahoo.es>
2020-07-09 21:20:04 -0600

    Arreglos en CSS

--- js/JS.js
+++ js/JS.js
@@ -2,22 +2,3 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    dots: false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
