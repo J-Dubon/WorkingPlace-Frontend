a909cb9bebbb92226dcfe8a5062cb002a91aef7a
Jose Dubon <josemdubonm@yahoo.es>
2020-07-10 10:20:12 -0600

    Deshaciendo codigo basura

--- js/JS.js
+++ js/JS.js
@@ -1,3 +1,14 @@
+
+AOS.init({
+    duration: 800,
+    ease: 'ease-in-out-sine'
+});
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 900,
+    speedAsDuration: true
+});
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -12,14 +23,16 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
     }
-})
-
-AOS.init({
-    duration: 800,
-    ease: 'ease-in-out-sine'
 });
 
-var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 900,
-    speedAsDuration: true
+
+
+/*$(document).ready(function() { 
+    $('.itemNav').click(function(){
+        var $target = $('.navbar-collapse');
+        if($target.hasClass('in')){
+            $target.removeClass('in').height(0).css('overflow','hidden');                           
+        }
+    });			
 });
+*/
